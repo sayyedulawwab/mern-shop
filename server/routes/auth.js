428be8699ecf3f71e2b40c03803c573a735d4bd4
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-01 16:53:51 +0600

    add json web token to login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // REGISTER
 router.post('/register', async (req, res) => {
@@ -37,9 +38,18 @@ router.post('/login', async (req, res) => {
     OriginalPassword !== req.body.password &&
       res.status(401).json('Wrong credentials!');
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '3d' }
+    );
+
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (error) {
     res.status(500).json(error);
   }
