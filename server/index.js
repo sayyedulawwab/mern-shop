2da38089268bfce4f3ed186496540c361b619af5
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-01 16:44:03 +0600

    add register and login routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 dotenv.config();
 
 mongoose
@@ -15,7 +16,7 @@ mongoose
   });
 
 app.use(express.json());
-
+app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
---
+++ server/routes/auth.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+
+// REGISTER
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    !user && res.status(401).json('Wrong credentials!');
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password &&
+      res.status(401).json('Wrong credentials!');
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
