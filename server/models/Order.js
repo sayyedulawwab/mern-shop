23237e33925704a07f6b1fe74e73640096d42d51
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-02 16:45:09 +0600

    fix Order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -4,17 +4,19 @@ const OrderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
     products: [
-        productId:{
-            type: String
+      {
+        productId: {
+          type: String,
         },
-        quantity:{
-            type:Number,
-            defalut:1
-        }
+        quantity: {
+          type: Number,
+          defalut: 1,
+        },
+      },
     ],
-    amount: {type:Number, required:true},
-    address: {type:Object, required:true},
-    status: {type:String, default: "pending"}
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: 'pending' },
   },
   { timestamps: true }
 );
