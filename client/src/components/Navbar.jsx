56ea6f647648b7c70d2f45c28b4884af79e6aaf9
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-05 23:23:27 +0600

    add missing Links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -83,8 +83,12 @@ const Navbar = () => {
           <Logo>MERN SHOP</Logo>
         </Center>
         <Right>
-          <MenuItem>REGISTER</MenuItem>
-          <MenuItem>SIGN IN</MenuItem>
+          <Link to="/register">
+            <MenuItem>REGISTER</MenuItem>
+          </Link>
+          <Link to="/login">
+            <MenuItem>SIGN IN</MenuItem>
+          </Link>
           <Link to="/cart">
             <MenuItem>
               <Badge badgeContent={quantity} color="primary">
