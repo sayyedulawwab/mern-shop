6303940bac5af58b7408e8a7c4aad0095214ca5a
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-05 23:14:16 +0600

    add redux for user and products

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
+import { persistor, store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}></PersistGate>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
