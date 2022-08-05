79fd3d61a276b51e505c552888aa3f87a8a6cd26
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-05 23:16:19 +0600

    get token from localstorage

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
 
-const TOKEN = process.env.REACT_APP_JWT_TOKEN;
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
