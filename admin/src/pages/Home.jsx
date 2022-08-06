9f57dfd0f661f47ebe649b31a8312420a98f9cf4
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-06 17:39:25 +0600

    add home page

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
+import Chart from '../components/Chart';
+import FeaturedInfo from '../components/FeaturedInfo';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-
+import WidgetLg from '../components/WidgetLg';
+import WidgetSm from '../components/WidgetSm';
+import { userRequest } from '../requestMethods';
 const Container = styled.div`
   display: flex;
 `;
 const Wrapper = styled.div`
   flex: 5;
 `;
+const HomeWidgets = styled.div`
+  display: flex;
+  margin: 20px;
+`;
+
 const Home = () => {
+  const [userStats, setUserStats] = useState([]);
+
+  const MONTHS = useMemo(
+    () => [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get('/users/stats');
+        res.data.map(item =>
+          setUserStats(prev => [
+            ...prev,
+            { name: MONTHS[item._id - 1], 'Active User': item.total },
+          ])
+        );
+      } catch {}
+    };
+    getStats();
+  }, [MONTHS]);
   return (
     <Container>
       <Sidebar />
       <Wrapper>
         <Navbar />
-        <h1>Home</h1>
+        <FeaturedInfo />
+        <Chart
+          data={userStats}
+          title="User Analytics"
+          grid
+          dataKey="Active User"
+        />
+        <HomeWidgets>
+          <WidgetSm />
+          <WidgetLg />
+        </HomeWidgets>
       </Wrapper>
     </Container>
   );
