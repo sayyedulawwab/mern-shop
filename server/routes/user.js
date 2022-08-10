b2fa5334f8f1b5575664f1e7a8f695f1c043d3f6
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-10 17:26:33 +0600

    Fix: Rearrange routes to fix /api/users/stats

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,56 @@ const User = require('../models/User');
 const CryptoJS = require('crypto-js');
 const router = require('express').Router();
 
+//GET ALL USER
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const query = req.query.new;
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET USER STATS
+
+router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: '$createdAt' },
+        },
+      },
+      {
+        $group: {
+          _id: '$month',
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//GET USER
+router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // ADD admin
 router.post('/', verifyTokenAndAdmin, async (req, res) => {
   const newUser = new User({
@@ -55,57 +105,6 @@ router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json('User has been deleted...');
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//GET USER
-router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//GET ALL USER
-router.get('/', verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const query = req.query.new;
-    const users = query
-      ? await User.find().sort({ _id: -1 }).limit(5)
-      : await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//GET USER STATS
-
-router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
-  const date = new Date();
-  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-  try {
-    const data = await User.aggregate([
-      { $match: { createdAt: { $gte: lastYear } } },
-      {
-        $project: {
-          month: { $month: '$createdAt' },
-        },
-      },
-      {
-        $group: {
-          _id: '$month',
-          total: { $sum: 1 },
-        },
-      },
-    ]);
-    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
