99dd0b3298dbd8f87dab57dbab0272e9a4d27726
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-08-11 20:51:48 +0600

    Fix: enable customer to access user info

--- server/routes/user.js
+++ server/routes/user.js
@@ -46,7 +46,7 @@ router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
   }
 });
 //GET USER
-router.get('/:id', verifyTokenAndAdmin, async (req, res) => {
+router.get('/:id', verifyTokenAndAuthorization, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
